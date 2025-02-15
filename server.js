6f7c539dc17e6f4664de3de76fe2f8d2e71ea4a4
saraalmheiri5 <g00092087@aus.edu>
2025-02-15 21:43:32 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 // Azure provides the PORT dynamically
 const PORT = process.env.PORT || 8080;
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route for the root URL
 app.get("/", (req, res) => {
-  res.send("Hello from Azure!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
